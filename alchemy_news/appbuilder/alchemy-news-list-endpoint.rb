f43bc85c434b7e35fe62784c548e6191a3378de1
Michael Keeling <mkeeling@us.ibm.com>
2015-11-10 10:54:58 -0500

    Updated Alchemy News list and trend widgets with sample endpoints

---
+++ alchemy_news/appbuilder/alchemy-news-list-endpoint.rb
@@ -0,0 +1,52 @@
+news_url = "http://access.alchemyapi.com/calls/data/GetNews"
+
+news_params = {
+  :apikey => 'YOUR_API_KEY_HERE',
+  :outputMode => 'json',
+  :maxResults => params[:max] || 5
+}
+
+
+returns = [
+  "enriched.url.title",
+  "enriched.url.url",
+  "enriched.url.author",
+  "enriched.url.publicationDate",
+  "enriched.url.entities",
+  "enriched.url.docSentiment",
+  "enriched.url.concepts",
+  "enriched.url.taxonomy"
+]
+
+news_params[:return] = returns.join(",")
+
+
+lables = [
+  "schema.original.url",
+  "schema.enriched.docSentiment",
+  "schema.enriched.concepts",
+  "schema.enriched.keywords",
+  "schema.enriched.entities"
+]
+
+news_params[:label_format_string] = lables.join(",")
+
+
+news_params[:start] = params[:start] || "now"
+news_params[:end] = params[:end] || "now-7d"
+
+if params[:sentiment] == "positive" || params[:sentiment] == "negative"
+   news_params["q.enriched.url.docSentiment.type"] = params[:sentiment]
+end
+
+
+news_params["q.enriched.url"] = params[:query]
+
+# See the full list of searchable fields: http://docs.alchemyapi.com/docs/full-list-of-supported-news-api-fields
+
+response = HttpClient.request(news_url, query: news_params)
+
+# Returns the Alchemy News API response, parsed as a Ruby Object.  Good for debugging.
+# JSON.parse(response.body, symbolize_names: true)
+
+response.body
---
+++ alchemy_news/appbuilder/news-trend-endpont.rb
@@ -0,0 +1,29 @@
+news_url = "http://access.alchemyapi.com/calls/data/GetNews"
+
+news_params = {
+  :apikey => 'YOUR_API_KEY_HERE',
+  :outputMode => 'json',
+  :count => 10
+}
+
+# params = JSON.parse(params, symbolize_names: true)
+
+news_params[:start] = params[:start] || "now"
+news_params[:end] = params[:end] || "now-7d"
+news_params[:timeslice] = params[:timeSlice] || "1d"
+
+if params[:sentiment] == "positive" || params[:sentiment] == "negative"
+   news_params["q.enriched.url.docSentiment.type"] = params[:sentiment]
+end
+
+news_params["q.enriched.url"] = params[:query]
+
+# See the full list of searchable fields: http://docs.alchemyapi.com/docs/full-list-of-supported-news-api-fields
+# news_params["q.enriched.url.entities.entity"] = params[:query]
+
+response = HttpClient.request(news_url, query: news_params)
+
+# Returns the Alchemy News API response, parsed as a Ruby Object.  Good for debugging.
+# JSON.parse(response.body, symbolize_names: true)
+
+response.body
